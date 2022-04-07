2cd41c8ba46a4747ae52b145f49aa4772401b2d9
DipuKP2001 <dipukp602@gmail.com>
2022-04-07 13:50:12 +0530

    redux final

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,10 +2,15 @@ import { persistStore, persistReducer } from 'redux-persist'
 
 import ReduxPersist from './config/reduxPersist.config'
 import configureStore from './configureStore'
+import reducers from './reducers'
+import rootSaga from './sagas'
 
 
 const persistConfig = ReduxPersist.storeConfig
-const {store} = configureStore(persistConfig)
+
+const finalReducers = persistReducer(persistConfig,reducers)
+
+const {store} = configureStore(finalReducers,rootSaga)
 
 const persistor = persistStore(store)
 
