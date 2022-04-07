89e16c108d05d7600614143a0c0d56f724d3914a
DipuKP2001 <dipukp602@gmail.com>
2022-04-07 13:38:11 +0530

    history

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,10 +1,11 @@
 import { persistStore, persistReducer } from 'redux-persist'
 
+import ReduxPersist from './config/reduxPersist.config'
 import configureStore from './configureStore'
 
 
-
-const {store} = configureStore()
+const persistConfig = ReduxPersist.storeConfig
+const {store} = configureStore(persistConfig)
 
 const persistor = persistStore(store)
 
